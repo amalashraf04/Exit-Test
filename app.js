fb8dcec41a5ff8f5e40f3abdb14882c2eacfb44e
amal <amalashraf04@gmail.com>
2023-05-27 19:41:36 +0530

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyparser = require('body-parser');
 const app =express();
 const nodemailer = require('nodemailer');
-
+const user=require('../backend/model/user')
 
 
 app.use(cors());
@@ -12,11 +12,71 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
 const mongoose = require('./db')
-const api = require('./routes/api')
-app.use('/api',api)
+// const api = require('./routes/api')
+// app.use('/api',api)
 
 
-//hosting
+app.post('/api/email',async(req,res)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Method:GET,POST,PUT,DELETE")
+    const randomPin = Math.floor(1000 + Math.random() * 9000);
+    var data ={
+     email : req.body.email,
+     otp:randomPin
+    }
+    var authdb = new user(data)
+    authdb.save().then((data)=>{
+   
+     var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          user: 'amalashraf04@gmail.com',
+          pass: 'xwyqvkduyyedqfzj'
+      }
+   });
+   
+   var mailOptions = {
+      from: 'amalashraf04@gmail.com',
+      to: data.email,
+      subject: 'OTP',
+      text: `Your OTP is: ${data.otp}`
+   
+   };
+
+   transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+    } else {
+        console.log('Email sent successfully'+info.response);
+    }
+ });
+   
+   console.log(data)
+  res.send(data)
+  }
+   )
+ 
+ })
+ 
+ app.post('/api/otp',(req,res)=>{
+   var data = {
+     email:req.body.email,
+     otp:req.body.otp
+ }
+ user.findOne({email:data.email,otp:data.otp}).then((data)=>{
+  if (data != null ){
+   console.log("otp",data)
+   res.send(data)
+  }
+  else{
+ res.send(null)
+  }
+  
+ })
+ })
+
+
+hosting
 
 const path = require('path');
 app.use(express.static('./dist/frontend/'))
--- model/user.js
+++ model/user.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    name : {
-        type: String,
-    
-    },
+   
     email :{
         type: String,
         required : true
     },
     otp :{
-        type: String,
+        type: Number,
        
     }
 });
 
-const userInfo = mongoose.model('user', userSchema);
-module.exports = userInfo;
+const user = mongoose.model('user', userSchema);
+module.exports = user;
